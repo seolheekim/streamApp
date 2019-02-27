7630c66e49f056e4e32fc3e5264e6cc0629ff844
Seolhee Kim <seolheekim808@gmail.com>
2019-02-27 11:05:20 -0800

    creating google auth

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -6,7 +6,10 @@ class GoogleAuth extends Component {
       window.gapi.client.init({
         clientId: '358448226936-lpavvkuu6cpkop9shubdl4vlmgg15bju.apps.googleusercontent.com',
         scope: 'email'
-      });
+      }).then(() => {
+        this.auth = window.gapi.auth2.getAuthInstance();
+        this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+      })
     });
   };
 
@@ -28,11 +31,20 @@ export default GoogleAuth;
 //  'window.gapi.load(client:auth2);' is going to load up that
 //  client portion of the library.
 //
+//  when you call 'client.init' it executes an asynchronous operation or an network request
+//  over to Google API server in order to initialize client.
+//
+//  '.then()' after running this code here you can reference 'this.auth' any other function inside
+//  the class. 'this.auth' will give reference to the auth instance that can be used to sign the
+//  user in or sign them out.
+//
 //componentDidMount() {
 //   window.gapi.load('client:auth2', () => {
-//     window.gapi.clinet.init({
-//       clinetId: '358448226936-lpavvkuu6cpkop9shubdl4vlmgg15bju.apps.googleusercontent.com',
+//     window.gapi.client.init({
+//       clientId: '358448226936-lpavvkuu6cpkop9shubdl4vlmgg15bju.apps.googleusercontent.com',
 //       scope: 'email'
-//     });
+//     }).then(() => {
+//       this.auth = window.gapi.auth2.getAuthInstance();
+//     })
 //   });
-// }
+// };
