12787fb0c63726bf116ab77ddebf7ff995f6bdf7
Seolhee Kim <seolheekim808@gmail.com>
2019-03-01 16:59:16 -0800

    google auth setup also able to get userId

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -4,8 +4,6 @@ import { signIn, signOut }    from '../actions';
 
 
 class GoogleAuth extends Component {
-  state = { isSignedIn: null };
-
   componentDidMount() {
     window.gapi.load('client:auth2', () => {
       window.gapi.client.init({
@@ -13,15 +11,16 @@ class GoogleAuth extends Component {
         scope: 'email'
       }).then(() => {
         this.auth = window.gapi.auth2.getAuthInstance();
-        this.setState({ isSignedIn: this.auth.isSignedIn.get() })
-        this.auth.isSignedIn.listen(this.onAuthChange)
+
+        this.onAuthChange(this.auth.isSignedIn.get());
+        this.auth.isSignedIn.listen(this.onAuthChange);
       })
     });
   };
 
   onAuthChange = (isSignedIn) => {
     if(isSignedIn){
-      this.props.signIn();
+      this.props.signIn(this.auth.currentUser.get().getId());
     } else {
       this.props.signOut();
     }
@@ -36,9 +35,9 @@ class GoogleAuth extends Component {
   };
 
   renderAuthButton() {
-    if(this.state.isSignedIn === null) {
+    if(this.props.isSignedIn === null) {
       return null;
-    } else if(this.state.isSignedIn) {
+    } else if(this.props.isSignedIn) {
       return (
         <button onClick={this.onSignOutClick} className="ui green google button">
           <i className="google icon" />
@@ -62,8 +61,12 @@ class GoogleAuth extends Component {
   }
 }; // end of GoogleAuth()
 
+const mapStateToProps = (state) => {
+  return { isSignedIn: state.auth.isSignedIn }
+}
+
 export default connect(
-  null,
+  mapStateToProps,
   { signIn, signOut }
 )(GoogleAuth);
 
--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,18 +1,20 @@
+import { SIGN_IN, SIGN_OUT } from '../actions/types'
 
 // Initialiser that will have isSignedIn property to null.
 // True constant object and it should never be changed.
 const INITAL_STATE = {
-  isSignedIn: null
+  isSignedIn: null,
+  userId: null
 };
 
 // (REMINDER) when redux first boots up the application
 // it gets called one time to initialize it.
 export default ( state = INITAL_STATE, action ) => {
   switch (action.type) {
-    case 'SIGN_IN':
-      return { ...state, isSignedIn: true }
-    case 'SIGN_OUT':
-      return { ...state, isSignedIn: false }
+    case SIGN_IN:
+      return { ...state, isSignedIn: true, userId: action.payload }
+    case SIGN_OUT:
+      return { ...state, isSignedIn: false, userId: null }
     default:
       return state;
   }
